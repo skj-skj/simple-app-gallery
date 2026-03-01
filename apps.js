80384fc2cdbbc5034b00c1367dd3cc7d79ec9a27
Sripal K Jain <paljain4444@gmail.com>
2026-03-01 19:14:37 +0530

    initial commit

---
+++ apps.js
@@ -0,0 +1,8 @@
+const APPS = [
+  {
+    dir: "apps/svg-to-png",
+    title: "SVG to PNG",
+    description: "Simple SVG to PNG Convertor",
+    tags: ["svg", "png", "image", "conversion", "convertor"]
+  },
+];
