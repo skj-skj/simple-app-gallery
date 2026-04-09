d5e556fa0b906289ca94df642372d58145fb7cc9
Sripal K Jain <paljain4444@gmail.com>
2026-04-10 01:17:46 +0530

    adds otpauth migration decoder app

--- apps.js
+++ apps.js
@@ -4,5 +4,10 @@ const APPS = [
     title: "SVG to PNG",
     description: "Simple SVG to PNG Convertor",
     tags: ["svg", "png", "image", "conversion", "convertor"]
+  },{
+    dir: "apps/otpauth-migration-decoder",
+    title: "OTPAuth Migration Decoder",
+    description: "Simple Google Authenticator OTPAuth Migration decoder",
+    tags: ["authenticator", "otp", "migration", "decoder"]
   },
 ];
